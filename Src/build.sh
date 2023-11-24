d80b30ecaf11c4dbf50077f2ffbff5087ee869f3
Andre-Cmd-Rgb <andreavigolini@gmail.com>
2023-11-24 18:30:35 +0100

    Better build file

--- Src/build.sh
+++ Src/build.sh
@@ -1,6 +1,41 @@
-nasm -f elf32 bootloader.asm -o bootloader.o
-gcc -m32 -c kernel.c -o kernel.o
-gcc -m32 -c libs/graphics.c -o graphics.o
-ld -m elf_i386 -T link.ld -o Grub/boot/kernel.bin bootloader.o kernel.o
-grub-mkrescue -o OS.iso Build/
-qemu-system-x86_64 -kernel Grub/boot/kernel.bin
+#!/bin/bash
+set -e   Exit on error
+
+# Source files
+BOOTLOADER_SRC="bootloader.asm"
+KERNEL_SRC="kernel.c"
+GRAPHICS_SRC="libs/graphics.c"
+
+# Output files
+BUILD_DIR="Build"
+BOOTLOADER_OBJ="$BUILD_DIR/bootloader.o"
+KERNEL_OBJ="$BUILD_DIR/kernel.o"
+GRAPHICS_OBJ="$BUILD_DIR/graphics.o"
+KERNEL_BIN="$BUILD_DIR/kernel.bin"
+
+# Compiler and linker flags
+NASM="nasm -f elf32"
+GCC="gcc -m32"
+LD="ld -m elf_i386 -T link.ld"
+
+# Create Build directory if it doesn't exist
+mkdir -p $BUILD_DIR
+
+# Build bootloader
+$NASM $BOOTLOADER_SRC -o $BOOTLOADER_OBJ
+
+# Build kernel and graphics library
+$GCC -c $KERNEL_SRC -o $KERNEL_OBJ
+$GCC -c $GRAPHICS_SRC -o $GRAPHICS_OBJ
+
+# Linking
+$LD -o $KERNEL_BIN $BOOTLOADER_OBJ $KERNEL_OBJ
+
+# Clean up unnecessary object files
+rm $BOOTLOADER_OBJ $KERNEL_OBJ $GRAPHICS_OBJ
+
+# Build ISO (uncomment if needed)
+#grub-mkrescue -o $BUILD_DIR/OS.iso Grub/
+
+# Run in QEMU
+qemu-system-x86_64 -kernel $KERNEL_BIN
