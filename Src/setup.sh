f55d484dff0bdf39a16872d0f69bbd8ccbc13100
Andrew <85292605+Andre-cmd-rgb@users.noreply.github.com>
2023-11-25 11:38:38 +0100

    Update setup.sh

--- Src/setup.sh
+++ Src/setup.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install xorriso
-sudo apt install grub
+sudo apt install grub-common
 sudo apt install gcc
 sudo apt install nasm 
-sudo apt install virtualbox
+#sudo apt install virtualbox
 sudo apt install qemu
